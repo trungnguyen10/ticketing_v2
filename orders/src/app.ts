d0d1888fb546d71c2e916624cd258f48ab573d56
Trung Nguyen <trungnguyen2809@gmail.com>
2024-11-15 20:47:13 -0600

    experiment with mongo replica set
    This reverts commit f154685f2efb207cf24c4a6bba20483c57e3c95f.

---
+++ orders/src/app.ts
@@ -0,0 +1,51 @@
+import {
+  currentUserHandler,
+  errorHandler,
+  GenericErrorMapper,
+  InvalidErrorMapper,
+  NotFoundError,
+  NotFoundErrorMapper,
+  ResourceExistsErrorMapper,
+  UnauthorizedErrorMapper,
+  ValidationErrorMapper,
+} from '@tnticketingdev/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+import { HttpProblemResponse } from 'express-http-problem-details';
+import {
+  DefaultMappingStrategy,
+  MapperRegistry,
+} from 'http-problem-details-mapper';
+
+const strategy = new DefaultMappingStrategy(
+  new MapperRegistry({ useDefaultErrorMapper: false })
+    .registerMapper(new GenericErrorMapper())
+    .registerMapper(new ValidationErrorMapper())
+    .registerMapper(new NotFoundErrorMapper())
+    .registerMapper(new ResourceExistsErrorMapper())
+    .registerMapper(new InvalidErrorMapper())
+    .registerMapper(new UnauthorizedErrorMapper())
+);
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+app.use(currentUserHandler);
+
+app.use('/', (req, res) => {
+  res.send('hello from tickets');
+});
+app.all('*', async (_req, _res, next) => {
+  next(new NotFoundError());
+});
+app.use(errorHandler);
+app.use(HttpProblemResponse({ strategy }));
+
+export { app };
